5a4a6a36bc4adeb07d36a88f20620e556c6691e3
priyotri <ptriwinarko33@gmail.com>
2020-06-25 20:53:09 +0700

    menambahkan file index,php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>AKU SAYANG KAMU</h1>
+    <ul>
+        <li>AKU SAYANG KAMU</li>
+        <li>AKU SAYANG KAMU</li>
+        <li>AKU SAYANG KAMU</li>
+        <li>AKU SAYANG KAMU</li>
+        <li>AKU SAYANG KAMU</li>
+        <li>AKU SAYANG KAMU</li>
+        <li>AKU SAYANG KAMU</li>
+        <li>AKU SAYANG KAMU</li>
+        <li>AKU SAYANG KAMU</li>
+    </ul>
+</body>
+
+</html>
